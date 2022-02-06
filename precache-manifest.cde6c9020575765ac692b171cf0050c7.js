537711a775e45d3fc093614256212453d8246478
kururu-abdo <wdalhajana50@gmail.com>
2022-02-06 17:14:07 +0200

    Updates

---
+++ precache-manifest.cde6c9020575765ac692b171cf0050c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860f87a754f95b565ec63dfe541e85a2",
+    "url": "/abdoutMe/index.html"
+  },
+  {
+    "revision": "c6c763bb9d995e904c62",
+    "url": "/abdoutMe/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "a5f48a1daf5042ae8772",
+    "url": "/abdoutMe/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "c6c763bb9d995e904c62",
+    "url": "/abdoutMe/static/js/2.76c5f493.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/abdoutMe/static/js/2.76c5f493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5f48a1daf5042ae8772",
+    "url": "/abdoutMe/static/js/main.5f0d53cf.chunk.js"
+  },
+  {
+    "revision": "577a3b2ba004fdabfb73",
+    "url": "/abdoutMe/static/js/runtime-main.3118bdc3.js"
+  }
+]);
